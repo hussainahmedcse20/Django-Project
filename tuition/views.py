f258527cbb5324b19fb665f289c2ff922912f692
Hussain Ahmed <hussainahmedcse@gmail.com>
2021-10-09 14:54:37 +0600

    Added profile insert

--- tuition/views.py
+++ tuition/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from tuition.models import profile
 
 def home(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        phone_no = request.POST['phone_no']
+        message = request.POST['message']
+        pro= profile(name=name,email=email,password=password,phone_no=phone_no,message=message)
+        pro.save()
     return render(request, 'index.html')
